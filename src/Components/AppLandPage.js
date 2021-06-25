12adca193c280ab9a7fc5005b3d240d1be72346a
N-Akoma <yannsiyoung@gmail.com>
2021-06-25 14:24:25 +0100

    final year project

---
+++ src/Components/AppLandPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import Image from '../Images/logo.jpg'
+
+const AppLandPage = () => {
+	return (
+		<div className="container col-11 p-3">
+			<h5 className='lead nav-text'>Weekly Budget Assistance</h5>
+			<div className='p'>
+			<h1 className='nav-text text-light font-weight-bold'>Saving Money</h1>
+			</div>
+		</div>
+	);
+};
+
+export default AppLandPage;
